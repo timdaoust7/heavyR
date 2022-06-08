b4528eb084dd4159417a209b7ad0fbc5ff8eb70d
parkseeh <parkseeh0226@gmail.com>
2022-06-08 14:12:39 +0200

    Initial commit

---
+++ R/mvndpf.R
@@ -0,0 +1,37 @@
+#' @title Multivariate functions
+#'
+#' @description  it calculate the multivariate normal distribution functions
+#'
+#' @param x A matrix
+#' @param mean A mena value of matrix
+#' @param varcovM variance-covariance matrix
+#' @param Log Calculate log value, default is TRUE,
+#'
+#' @return A list containing the
+#' @export
+#'
+#' @examples
+mvnpdf <- function(x, mean =  rep(0, nrow(x)),
+                   varcovM = diag(nrow(x)), Log = TRUE) {
+    n <- ncol(x)
+    p <- nrow(x)
+    x0 <- x - mean
+    Rinv <- solve(varcovM)
+    LogDetvarcovM <- log(det(varcovM))
+
+    y <- NULL
+    for (j in 1:n) {
+        yj <- - p/2 * log(2*pi) - 0.5 * LogDetvarcovM - 0.5 * t(x0[, j]) %*% Rinv %*% x0[, j]
+        y <- c(y, yj)
+    }
+
+    if (!Log) {
+        y <- exp(y)
+    }
+
+    res <- list(x = x, y = y)
+    #class(res) <- "mvnpdf"
+    return(res)
+}
+
+
